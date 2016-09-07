a401d811616dc17708e041c73dfa04e00e296bca
armaanrustami <armaan.rustami@gmail.com>
2016-09-07 21:48:44 +0200

    Dirrectory Creation

---
+++ Week2/2_Assignment/CMakeFiles/cppInterface.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cppInterface.dir/main.cpp.o"
+  "cppInterface.pdb"
+  "cppInterface"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cppInterface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
